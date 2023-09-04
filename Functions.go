5934c84753b05b7099a04807b4e01bc1b81423fd
Andoni Salcedo Navarro <andonisalcedo@gmail.com>
2023-09-04 14:48:00 +0200

    documentación

--- Functions.go
+++ Functions.go
@@ -1,11 +1,16 @@
 package sidecar
 
+// This file defines two custom function types for handling HTTP requests and responses in different scenarios.
+
+// Importing the required packages.
 import (
 	"net/http"
 
 	cloudevents "github.com/cloudevents/sdk-go/v2"
 )
 
+// TriFunction is a type representing a function that takes in an HTTP request, an HTTP response writer, and a FilterChain.
 type TriFunction func(req *http.Request, res http.ResponseWriter, chain *FilterChain)
 
+// QuaFunction is a type representing a function that takes in an HTTP request, an HTTP response writer, a cloud event, and a FilterChain.
 type QuaFunction func(*http.Request, http.ResponseWriter, cloudevents.Event, *FilterChain)
