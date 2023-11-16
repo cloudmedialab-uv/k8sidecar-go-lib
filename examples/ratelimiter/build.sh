6a1257c8e3436b88efe0616df8cd50fe7b33076b
Andoni Salcedo Navarro <ansalna@alumni.uv.es>
2023-11-16 13:18:50 +0100

    docker ratelimiter example

---
+++ examples/ratelimiter/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+TAG="1.0.0"
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+echo $SCRIPT_DIR
+
+docker build $SCRIPT_DIR -t k8sidecar/examples/ratelimiter:$TAG -f $SCRIPT_DIR/Dockerfile
+
+#docker tag k8sidecar/examples/ratelimiter:$TAG 
