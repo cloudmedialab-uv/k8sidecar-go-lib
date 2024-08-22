bdd72394aa9c7c4f3c96ef1d616c8cd19c8ef7de
Juan Gutiérrez-Aguado <juan.gutierrez@uv.es>
2024-08-22 10:42:48 +0200

    Add src folder in call to docker build

--- examples/ratelimiter/build.sh
+++ examples/ratelimiter/build.sh
@@ -9,7 +9,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 echo $SCRIPT_DIR
 
-docker build $SCRIPT_DIR -t k8sidecar/examples/ratelimiter:$TAG -f $SCRIPT_DIR/src/Dockerfile
+docker build $SCRIPT_DIR/src -t k8sidecar/examples/ratelimiter:$TAG -f $SCRIPT_DIR/src/Dockerfile
 
 docker tag k8sidecar/examples/ratelimiter:$TAG $REPO_RATELIMITER:$TAG
 
